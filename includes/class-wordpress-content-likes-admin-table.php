6cef1a6c0f9357437f7c18b739606e4cf0376268
radboris <rad.borislavov@gmail.com>
2019-09-23 12:31:13 -0500

    update adin table

--- includes/class-wordpress-content-likes-admin-table.php
+++ includes/class-wordpress-content-likes-admin-table.php
@@ -3,6 +3,7 @@
 if (!class_exists('WP_List_Table')) {
     require_once ABSPATH.'wp-admin/includes/class-wp-list-table.php';
 }
+require_once 'class-wordpress-content-likes-query-content.php';
 
 class AdminTable extends WP_List_Table
 {
@@ -83,60 +84,6 @@ class AdminTable extends WP_List_Table
         );
 
         return $sortable_columns;
-    }
-
-    public function getCustomPostsLikes()
-    {
-        global $wpdb;
-        $pref = $wpdb->prefix;
-
-        $query = "SELECT post_id, meta_value AS LIKES, POST_TITLE from {$pref}postmeta
-            LEFT JOIN {$pref}posts  on {$pref}posts.ID = {$pref}postmeta.post_id
-                where  meta_value = (
-                    select MAX(meta_value) from  {$pref}postmeta where meta_key = 'likes'
-                )
-            and meta_key = 'likes'
-            and {$pref}posts.post_type IN ({$custom_posts})";
-
-        $the_max = $wpdb->get_row($query);
-
-        return $the_max;
-    }
-
-    public function getPostsLikes()
-    {
-        global $wpdb;
-        $pref = $wpdb->prefix;
-
-        $query = "SELECT post_id, meta_value AS LIKES, POST_TITLE from {$pref}postmeta
-            LEFT JOIN {$pref}posts  on {$pref}posts.ID = {$pref}postmeta.post_id
-                where  meta_value = (
-                    select MAX(meta_value) from  {$pref}postmeta where meta_key = 'likes'
-                )
-            and meta_key = 'likes'
-            and {$pref}posts.post_type = 'post'";
-
-        $the_max_posts = $wpdb->get_row($query);
-
-        return $the_max_posts;
-    }
-
-    public function getPagesLikes()
-    {
-        global $wpdb;
-        $pref = $wpdb->prefix;
-
-        $query = "SELECT post_id, meta_value AS LIKES, POST_TITLE from {$pref}postmeta
-            LEFT JOIN {$pref}posts  on {$pref}posts.ID = {$pref}postmeta.post_id
-                where  meta_value = (
-                    select MAX(meta_value) from  {$pref}postmeta where meta_key = 'likes'
-                )
-            and meta_key = 'likes'
-            and {$pref}posts.post_type = 'page'";
-
-        $the_max_pages = $wpdb->get_row($query);
-
-        return $the_max_pages;
     }
 
     public function no_items()
